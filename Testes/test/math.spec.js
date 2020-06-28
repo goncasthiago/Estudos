f7248d04007921770fe1c60029ffea3dd11a9288
goncasthiago <thiagodebia@gmail.com>
2020-06-28 15:49:44 -0300

    Testes com Chai

--- Testes/test/math.spec.js
+++ Testes/test/math.spec.js
@@ -1,4 +1,5 @@
 const assert = require ('assert');
+const expect = require('chai').expect;
 const Math = require('../src/math')
 
 describe('Math class', function(){
@@ -14,13 +15,27 @@ describe('Math class', function(){
         this.timeout(3000);
         value =5;
         math.sum(value, 5, value => {
-            assert.equal(value, 10)
+            expect(value).to.equal(10); //usando o chai
+            //assert.equal(value, 10) //usando o mocha
             done()
         })
   })
   it('Multiply two numbers', function() {
 
       const math = new Math();
-      assert.equal(math.multiply(value,5),0);
+        expect(math.multiply(value,5)).to.equal(0); //usando o chai
+      //assert.equal(math.multiply(value,5),0); //usando o mocha
+  })
+
+  //testando com objetos
+  // quando comparamos dois objetos utilizamos o
+  //expect(obj).to.deep.equal(obj2);
+  it('Have a name', function() {
+    const math = new Math();
+    const obj = {
+        name: 'Thiago Debia'
+    }
+    expect(obj).to.have.property('name').equal('Thiago Debia')
+
   })
 })
